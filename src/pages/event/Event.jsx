40a81bedfa31fe0cf3d98ea974cb789a03441577
Jorge Matos <jisapjisap@gmail.com>
2026-08-20 18:49:51 +0200

    feat: implement Event page and reusable EventCard component

--- src/pages/event/Event.jsx
+++ src/pages/event/Event.jsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import Breadcrumb from "@components/ui/Breadcrumb";
+import EventCard from "@components/cards/EventCard";
+import { events } from "@data/events";
+import { staggerDelay } from "@utils/helpers";
+
+import eventElement from "@assets/event-element.png";
+import eventBlink from "@assets/event-blink.svg";
 
 const Event = () => {
     return (
