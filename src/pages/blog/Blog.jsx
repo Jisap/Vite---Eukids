067fbeed0e438819e7527c03865f59ec17f03903
Jorge Matos <jisapjisap@gmail.com>
2026-08-21 12:38:35 +0200

    feat: add blog listing page and individual blog post details view

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { useMemo, useState } from "react";
+import { Icon } from "@iconify/react";
+import Breadcrumb from "@components/ui/Breadcrumb";
+import BlogCard from "@components/cards/BlogCard";
+import { blogs } from "@data/blogs";
+import { staggerDelay } from "@utils/helpers";
+
+const VIEW_OPTIONS = [
+  { value: "grid", icon: "mdi:view-grid-outline" },
+  { value: "list", icon: "mdi:view-list" },
+];
+
 
 const Blog = () => {
-    return (
-        <div>Blog</div>
-    )
+
+  const [view, setView] = useState("grid");
+  const [activeCategory, setActiveCategory] = useState("All");
+
+  const categories = useMemo(() => ["All", ...new Set(blogs.map((b) => b.category))], []); // Array con las categorías
+
+  const filteredBlogs =
+    activeCategory === "All"
+      ? blogs
+      : blogs.filter((b) => b.category === activeCategory);
+
+  return (
+    <div>Blog</div>
+  )
 }
 
 export default Blog
