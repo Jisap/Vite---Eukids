90199958491d72bcdeda76aa41cd70857724bd8f
Jorge Matos <jisapjisap@gmail.com>
2026-08-24 12:51:13 +0200

    feat: create initial Pricing page layout with breadcrumb component

--- src/pages/pricing/Pricing.jsx
+++ src/pages/pricing/Pricing.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import Breadcrumb from "@components/ui/Breadcrumb";
+import SectionHeading from "@components/ui/SectionHeading";
+import PricingCard from "@components/cards/PricingCard";
+import { pricingPlans } from "@data/pricing";
+import { staggerDelay } from "@utils/helpers";
 
 const Pricing = () => {
-    return (
-        <div>Pricing</div>
-    )
+  return (
+    <>
+      <Breadcrumb title="Pricing Plans" currentPage="Pricing" />
+
+    </>
+  )
 }
 
 export default Pricing
